8ad9a37936c5501c5dcf89a25a541ac08d708b4d
aoineco <masaya891047@gmail.com>
2022-07-11 17:58:46 +0900

    Added checkboxes for diet and activity

--- select_choice.php
+++ select_choice.php
@@ -9,19 +9,47 @@
 </head>
 
 <body>
+  TODO:食べた量を選択する（たくさん、少し、食べてない）
+  TODO:それぞれの点数を追加
+  TODO:コードを整理
   <form method="POST" action="hoge.php" name="test">
-    <div class="foods you ate">
-      <input type="checkbox" id=1 class="test" value="2" checked>
-      <input type="checkbox" id=1 class="test" value="2" checked>
-      <input type="checkbox" id=1 class="test" value="2" checked>
-
-
+    <div class="O">
+      <!-- FODMAP foods (Oligosaccharides) -->
+      <input type="checkbox" id=1 class="test" value="2">beans<br>
+      <input type="checkbox" id=1 class="test" value="2">onion<br>
+      <input type="checkbox" id=1 class="test" value="2">Cooking with Wheat Ingredients<br>
     </div>
-    <div class="your ativity">
-      <input type="checkbox" name="test">
-
+    <div class="D">
+      <!-- FODMAP foods (Disaccharides) -->
+      <input type="checkbox" id=1 class="test" value="2">fruits<br>
+      <input type="checkbox" id=1 class="test" value="2">honey<br>
     </div>
-    <input type="submit" value="submit">
+    <div class="M">
+      <!-- FODMAP foods (Monosaccharides) -->
+      <input type="checkbox" id=1 class="test" value="2">mushroomss<br>
+      <input type="checkbox" id=1 class="test" value="2">onion<br>
+      <input type="checkbox" id=1 class="test" value="2">artificial sweetener<br>
+    </div>
+    <div class="P">
+      <!-- FODMAP foods (Polyols) -->
+      <input type="checkbox" id=1 class="test" value="2">beans<br>
+      <input type="checkbox" id=1 class="test" value="2">onion<br>
+      <input type="checkbox" id=1 class="test" value="2">Cooking with Wheat Ingredients<br>
+    </div>
+    <div class="other_foods">
+      <input type="checkbox">meat <br>
+      <input type="checkbox">egg <br>
+    </div>
+    <div class="fitness">
+      <input type="checkbox">exercise <br>
+    </div>
+    <div class="sleep">
+      <input type="checkbox"> Sleep Satisfaction<br>
+    </div>
+    <div class="stress">
+      <input type="checkbox"> stress<br>
+    </div>
+
   </form>
 
   <?php
